101002e44cfc84d8abf57b7e0bf1c640157cdace
Bee <xronsredstone@gmail.com>
2022-09-25 11:16:28 -0400

    got the buttons working

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
 var startBtn = document.getElementById('start');
+var hsBtn = document.getElementById('hs');
 startBtn.addEventListener("click", startGame);
+hsBtn.addEventListener("click", highScore);
+
 
 function startGame(){
-console.log ("yeeeee");
+    console.log("press the button to begin")
 }
+
+
+
+function highScore(){
+    console.log("press the button to begin")
+}
+
