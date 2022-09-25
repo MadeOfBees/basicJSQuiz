ec583c662adc3b5e5d456f4578b106f23502b05a
Bee <xronsredstone@gmail.com>
2022-09-25 11:26:34 -0400

    my first easter egg

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,23 @@
 var startBtn = document.getElementById('start');
 var hsBtn = document.getElementById('hs');
+var beeBtn = document.getElementById('bee');
 startBtn.addEventListener("click", startGame);
 hsBtn.addEventListener("click", highScore);
-
+beeBtn.addEventListener("click", buzzBuzz);
 
 function startGame(){
     console.log("press the button to begin")
 }
 
-
-
 function highScore(){
-    console.log("press the button to begin")
+    console.log("the other button")
+}
+
+function buzzBuzz(){
+    console.log("                .' '.            __");
+    console.log("       .        .   .           (__\_");
+    console.log("        .         .         . -{{_(|8)");
+    console.log("          ' .  . ' ' .  . '     (__/" );
+    console.log("ACSII BY LOAN STARK ON ASCII ART ARCHIVE");
 }
 
