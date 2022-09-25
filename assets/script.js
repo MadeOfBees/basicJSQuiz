b48934e3170c3f10d9967d273048f9e72092bbfd
Bee <xronsredstone@gmail.com>
2022-09-25 15:31:17 -0400

    still not fixed but dang did I try

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
 var startBtn = document.getElementById('start');
 var hsBtn = document.getElementById('hs');
 // Ignore line 4
-document.getElementById('bee').addEventListener("click", function(){console.log("          __         .' '.        ");console.log("        _/__)        .   .       .");console.log("       (8|)_}}- .      .        . ");console.log("        `\\__)    '. . ' ' .  . ' ");console.log("Bzzz site MadeOfBees              ");});
+document.getElementById('bee').addEventListener("click", function(){console.log("          __         .' '.");console.log("        _/__)        .   .       .");console.log("       (8|)_}}- .      .        .");console.log("        `\\__)    '. . ' ' .  . ' ");console.log("       Bzzz site MadeOfBees");});
 var title = document.getElementById('tScreen');
 startBtn.addEventListener("click", startGame);
 hsBtn.addEventListener("click", highScore);
 
 title.text("TITLE SCREEN");
 
-function startGame(){
+function startGame(title){
     title.text("PLEASE");
 }
 
