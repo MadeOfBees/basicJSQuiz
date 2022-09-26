aacb82b135a5cdf76fc68c95564c06d86587713b
Bee <xronsredstone@gmail.com>
2022-09-26 10:42:05 -0400

    I fear no man, but that thing *points to JSQuery* it scares me

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,82 @@
-var startBtn = document.getElementById('start');
-var hsBtn = document.getElementById('hs');
-// Ignore line 4
-document.getElementById('bee').addEventListener("click", function(){console.log("          __         .' '.");console.log("        _/__)        .   .       .");console.log("       (8|)_}}- .      .        .");console.log("        `\\__)    '. . ' ' .  . ' ");console.log("       Bzzz site MadeOfBees");});
-var title = document.getElementById('tScreen');
+document.getElementById('bee').addEventListener("click", function () { console.log("          __         .' '."); console.log("        _/__)        .   .       ."); console.log("       (8|)_}}- .      .        ."); console.log("        `\\__)    '. . ' ' .  . ' "); console.log("       Bzzz site MadeOfBees"); });
+var playerScores;
+var gameState = "Super JSquiz Bros"
+var mainScreen = document.getElementById('mScreen');
+var mainScreen = document.getElementById('lScreen');
 startBtn.addEventListener("click", startGame);
 hsBtn.addEventListener("click", highScore);
-var playerScores;
-var gameState = "TITLESCREEN"
+var startBtn = document.getElementById('start');
+var hsBtn = document.getElementById('hs');
+var qNA = [
+    {
+        "ques": "what is 1+2",
+        "ans1": "minivan",
+        "ans2": "minivan",
+        "ans3": "3",
+        "ans4": "minivan",
+    },
+    {
+        "ques": "what is 1+2",
+        "ans1": "minivan",
+        "ans2": "6",
+        "ans3": "3",
+        "ans4": "minivan",
+    },
+    {
+        "ques": "what is 1+2",
+        "ans1": "minivan",
+        "ans2": "minivan",
+        "ans3": "3",
+        "ans4": "minivan",
+    },
+    {
+        "ques": "what is 1+2",
+        "ans1": "minivan",
+        "ans2": "3",
+        "ans3": "7",
+        "ans4": "minivan",
+    },
+    {
+        "ques": "what is 1+2",
+        "ans1": "4",
+        "ans2": "minivan",
+        "ans3": "3",
+        "ans4": "minivan",
+    },
+    {
+        "ques": "what is 1+2",
+        "ans1": "minivan",
+        "ans2": "1",
+        "ans3": "3",
+        "ans4": "minivan",
+    },
+    {
+        "ques": "what is 1+2",
+        "ans1": "minivan",
+        "ans2": "minivan",
+        "ans3": "3",
+        "ans4": "minivan",
+    },
+    {
+        "ques": "what is 1+2",
+        "ans1": "minivan",
+        "ans2": "minivan",
+        "ans3": "3",
+        "ans4": "minivan",
+    }]
 
-title.append(gameState);
+    mainScreen.append(gameState);
 
-function startGame(gameState){
-
-    gameState = ("Welcome Player")
-    title.text(gameState);
+function startGame(gameState) {
+    gameState = ("Question")
+    mainScreen.textContent = (gameState);
+    if (i = 0, i > 6, i++) {
+        // print 6 answers from our answer 1 object in an array.
+        document.createElement()
+    }
 }
 
-function highScore(){
+function highScore(gameState) {
     gameState = ("High scores")
-
-    title.text(gameState);
+    mainScreen.textContent = (gameState);
 }
