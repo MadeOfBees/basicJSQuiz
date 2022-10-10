8feb45449ee0375a29cf7d26b8234cf482a9f113
Bee <xronsredstone@gmail.com>
2022-10-10 10:13:49 -0400

    renders questions now

--- assets/script.js
+++ assets/script.js
@@ -1,82 +1,47 @@
-document.getElementById('bee').addEventListener("click", function () { console.log("          __         .' '."); console.log("        _/__)        .   .       ."); console.log("       (8|)_}}- .      .        ."); console.log("        `\\__)    '. . ' ' .  . ' "); console.log("       Bzzz site MadeOfBees"); });
 var playerScores;
 var gameState = "Super JSquiz Bros"
 var mainScreen = document.getElementById('mScreen');
 var mainScreen = document.getElementById('lScreen');
-startBtn.addEventListener("click", startGame);
-hsBtn.addEventListener("click", highScore);
 var startBtn = document.getElementById('start');
 var hsBtn = document.getElementById('hs');
-var qNA = [
-    {
-        "ques": "what is 1+2",
-        "ans1": "minivan",
-        "ans2": "minivan",
-        "ans3": "3",
-        "ans4": "minivan",
-    },
-    {
-        "ques": "what is 1+2",
-        "ans1": "minivan",
-        "ans2": "6",
-        "ans3": "3",
-        "ans4": "minivan",
-    },
-    {
-        "ques": "what is 1+2",
-        "ans1": "minivan",
-        "ans2": "minivan",
-        "ans3": "3",
-        "ans4": "minivan",
-    },
-    {
-        "ques": "what is 1+2",
-        "ans1": "minivan",
-        "ans2": "3",
-        "ans3": "7",
-        "ans4": "minivan",
-    },
-    {
-        "ques": "what is 1+2",
-        "ans1": "4",
-        "ans2": "minivan",
-        "ans3": "3",
-        "ans4": "minivan",
-    },
-    {
-        "ques": "what is 1+2",
-        "ans1": "minivan",
-        "ans2": "1",
-        "ans3": "3",
-        "ans4": "minivan",
-    },
-    {
-        "ques": "what is 1+2",
-        "ans1": "minivan",
-        "ans2": "minivan",
-        "ans3": "3",
-        "ans4": "minivan",
-    },
-    {
-        "ques": "what is 1+2",
-        "ans1": "minivan",
-        "ans2": "minivan",
-        "ans3": "3",
-        "ans4": "minivan",
-    }]
-
-    mainScreen.append(gameState);
-
-function startGame(gameState) {
-    gameState = ("Question")
-    mainScreen.textContent = (gameState);
-    if (i = 0, i > 6, i++) {
-        // print 6 answers from our answer 1 object in an array.
-        document.createElement()
+var an1 = document.getElementById('a1');
+var an2 = document.getElementById('a2');
+var an3 = document.getElementById('a3');
+var an4 = document.getElementById('a4');
+startBtn.addEventListener("click", startGame);
+hsBtn.addEventListener("click", highScore);
+class Question {
+    constructor(question, ans1, ans2, ans3, ans4) {
+      this.question = question;
+      this.ans1 = ans1;
+      this.ans2 = ans2;
+      this.ans3 = ans3;
+      this.ans4 = ans4;
     }
 }
+const quest1 = new Question(`What is javascript?`, `Fish`, `Java`, `Tea`, `A language`)
+const quest2 = new Question(`What is javascript?`, `Fish`, `Java`, `Tea`, `A language`)
+const quest3 = new Question(`What is javascript?`, `Fish`, `Java`, `Tea`, `A language`)
+const quest4 = new Question(`What is javascript?`, `Fish`, `Java`, `Tea`, `A language`)
+const quest5 = new Question(`What is javascript?`, `Fish`, `Java`, `Tea`, `A language`)
+
+function init(){
+mainScreen.append(gameState);
+}
+
 
 function highScore(gameState) {
     gameState = ("High scores")
     mainScreen.textContent = (gameState);
 }
+
+function startGame(a1,a2,a3,a4){
+    mainScreen.replaceWith (quest1.question);
+    an1.replaceWith (quest1.ans1);
+    an2.replaceWith (quest1.ans2);
+    an3.replaceWith (quest1.ans3);
+    an4.replaceWith (quest1.ans4);
+}
+
+
+init();
