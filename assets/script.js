170480919b501e66ed348eaf50fb5f38822ad3d7
Bee <xronsredstone@gmail.com>
2022-10-10 10:50:35 -0400

    making this a multi-page app

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,26 @@ var an1 = document.getElementById('a1');
 var an2 = document.getElementById('a2');
 var an3 = document.getElementById('a3');
 var an4 = document.getElementById('a4');
+var high1 = document.getElementById('a1');
+var high2 = document.getElementById('a2');
+var high3 = document.getElementById('a3');
+var high4 = document.getElementById('a4');
+var high5 = document.getElementById('hs5');
+var high6 = document.getElementById('hs6');
+var high7 = document.getElementById('hs7');
+var high8 = document.getElementById('hs8');
+var high9 = document.getElementById('hs9');
+var high10 = document.getElementById('hs10');
+var hi1 = `J Romero`;
+var hi2 = `J Carmack`; 
+var hi3 = `T Hall`; 
+var hi4 = `R Prince`; 
+var hi5 = `A Carmack`; 
+var hi6 = `J Romero`;
+var hi7 = `T Hall`;
+var hi8 = `J Carmack`; 
+var hi9 = `S Peterson`;
+var hi10 = `K Cloud`;
 startBtn.addEventListener("click", startGame);
 hsBtn.addEventListener("click", highScore);
 class Question {
@@ -19,6 +39,7 @@ class Question {
       this.ans4 = ans4;
     }
 }
+
 const quest1 = new Question(`What is javascript?`, `Fish`, `Java`, `Tea`, `A language`)
 const quest2 = new Question(`What is javascript?`, `Fish`, `Java`, `Tea`, `A language`)
 const quest3 = new Question(`What is javascript?`, `Fish`, `Java`, `Tea`, `A language`)
@@ -26,21 +47,31 @@ const quest4 = new Question(`What is javascript?`, `Fish`, `Java`, `Tea`, `A lan
 const quest5 = new Question(`What is javascript?`, `Fish`, `Java`, `Tea`, `A language`)
 
 function init(){
-mainScreen.append(gameState);
+mainScreen.textContent(gameState);
 }
 
 
 function highScore(gameState) {
     gameState = ("High scores")
     mainScreen.textContent = (gameState);
+    high1.textContent(hi1);
+    high2.textContent(hi2);
+    high3.textContent(hi3);
+    high4.textContent(hi4);
+    high5.textContent(hi5);
+    high6.textContent(hi6);
+    high7.textContent(hi7);
+    high8.textContent(hi8);
+    high9.textContent(hi9);
+    high10.textContent(hi10);
 }
 
-function startGame(a1,a2,a3,a4){
-    mainScreen.replaceWith (quest1.question);
-    an1.replaceWith (quest1.ans1);
-    an2.replaceWith (quest1.ans2);
-    an3.replaceWith (quest1.ans3);
-    an4.replaceWith (quest1.ans4);
+function startGame(){
+    mainScreen.textContent(quest1.question);
+    an1.textContent(quest1.ans1);
+    an2.textContent(quest1.ans2);
+    an3.textContent(quest1.ans3);
+    an4.textContent(quest1.ans4);
 }
 
 
